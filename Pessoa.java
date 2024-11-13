ddd642412e8a6561c4c0ec5fdb1047fca46d88d2
Gustavo Nunes <ustivogndo@gmail.com>
2024-11-13 11:32:59 -0400

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,11 +1,17 @@
 public class Pessoa {
 
+    private int id;
     private String nome;
     private String email;
 
-    public Pessoa(String nome, String email){
+    public Pessoa(int id, String nome, String email){
+        this.id = id;
         this.nome = nome;
         this.email = email;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getNome() {
